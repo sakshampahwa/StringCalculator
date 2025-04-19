9f836d335555b3e527b1e791181ee243cb81d70c
sakshampahwa <sakshampahwa12@gmail.com>
2025-04-19 19:45:57 +0530

    Test Case to support different delimiters

--- spec/models/string_calculator_tdd_spec.rb
+++ spec/models/string_calculator_tdd_spec.rb
@@ -21,5 +21,10 @@ RSpec.describe StringCalculatorTdd, type: :model do
       calculator = StringCalculatorTdd.new
       expect(calculator.add("1\n2,3")).to eq(6)
     end
+
+    it 'supports different delimiters' do
+      calculator = StringCalculatorTdd.new
+      expect(calculator.add("//;\n1;2")).to eq(3)
+    end
   end
 end
